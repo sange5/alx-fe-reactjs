13badd550996166be542645d56c3194175d646db
sange5 <163669819+sange5@users.noreply.github.com>
2025-09-04 14:18:27 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Header from", "import MainContent from", "import Footer from
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
 
