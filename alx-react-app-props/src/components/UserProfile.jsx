6fa147a30a6cba731d02143665f599797f8a67a6
sange5 <163669819+sange5@users.noreply.github.com>
2025-09-04 17:10:18 +0300

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,6 +1,7 @@
 // src/components/UserProfile.jsx
 const UserProfile = (props) => {
-  return (
+  return 
+  useContext", "UserContext", "react"(
     <div className="user-profile" style={{ border: "1px solid #ddd", padding: "12px", borderRadius: "8px", width: "280px", marginBottom: "12px" }}>
       <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
