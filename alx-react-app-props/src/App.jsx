1537c4d71d47d9fd06f3dcfeabfef222c846d283
sange5 <163669819+sange5@users.noreply.github.com>
2025-09-04 17:07:58 +0300

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -39,7 +39,8 @@ function App() {
       {/* Use UserProfile with props */}
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <UserProfile name="Ray" age="22" bio="Passionate about coding and design" />
-
+       "UserContext", "UserContext.Provider", "value"
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
