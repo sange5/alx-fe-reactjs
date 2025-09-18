8d00bfb652fc8d4c05624d56decd6b2395f1d9e9
sange5 <163669819+sange5@users.noreply.github.com>
2025-09-18 16:23:26 +0300

    Create githubService.js

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.github.com/users/";
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${BASE_URL}${username}`, {
+      headers: {
+        Authorization: import.meta.env.VITE_APP_GITHUB_API_KEY
+          ? `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`
+          : undefined,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("User not found");
+  }
+};
+
