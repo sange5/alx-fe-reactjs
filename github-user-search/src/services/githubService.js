5f04b2b7fa923cbd53f282b0129af95ecb91b4c4
sange5 <163669819+sange5@users.noreply.github.com>
2025-09-18 17:01:13 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.github.com/users/";
+"https://api.github.com/search/users?q", "location", "minRepos"
 
 export const fetchUserData = async (username) => {
   try {
